3368d36fd424444d575e10ec447121375937ff95
Sky2016cn <58769170+sky2016cn@users.noreply.github.com>
2026-09-20 15:40:04 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -37,9 +37,6 @@ clone_if_missing https://github.com/Openwrt-Passwall/openwrt-passwall-packages "
 clone_if_missing https://github.com/Openwrt-Passwall/openwrt-passwall  ""     package/passwall-luci
 #clone_if_missing https://github.com/EasyTier/luci-app-easytier.git     ""     package/luci-app-easytier
 
-sed -i '/^GO_PKG:=github.com\/daeuniverse\/dae-wing$/a GO_PKG_INSTALL_EXTRA:=webrender/web' \
-  package/dae/daed/Makefile
-
 # 修改版本为编译日期
 DATE_VERSION="$(date +%Y.%m.%d)"
 VERSION_FILE="include/version.mk"
