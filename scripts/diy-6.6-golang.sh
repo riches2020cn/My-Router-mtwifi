1b1eb1bbb5c5288ac1e1943ac2fc63c12476c264
Sky2016cn <58769170+sky2016cn@users.noreply.github.com>
2026-09-23 15:43:06 +0800

    Update and rename diy-6.6.sh to diy-6.6-golang.sh

--- scripts/diy-6.6-golang.sh
+++ scripts/diy-6.6-golang.sh
@@ -10,8 +10,10 @@ cp -rf "$GOLANG126_SRC_DIR/." "$GOLANG126_FEED_DIR/"
 ./scripts/feeds update -f packages
 ./scripts/feeds install golang1.27
 
-# passwall daed use golang1.27/host（修改 package/dae 和 package/passwall-packages 目录下所有 Makefile 文件，指向 golang1.27）
-find package/dae package/passwall-packages -name "Makefile" -type f -exec sed -i \
+# passwall 和 daed 使用 golang1.27/host（修改拉取第三方 package/dae 和 package/passwall-packages 目录下所有 Makefile 文件，指向 golang1.27）
+#find package/dae package/passwall-packages -name "Makefile" -type f -exec sed -i \
+# 下方第一条命令修改自上方，中间少了一个“package/dae”，如要使用 luci-app-daed，请使用上方命令，还需要修改“diy-script.sh”脚本
+find package/passwall-packages -name "Makefile" -type f -exec sed -i \
   -e 's|\<golang/golang-package.mk\>|golang1.27/golang-package.mk|g' \
   -e 's|\<golang/host\>|golang1.27/host|g' {} +
 
